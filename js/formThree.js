7343804e75a79558de289a5c7980fb23f02269e2
Justin Duong <jduong@umich.edu>
2022-12-08 16:09:50 -0500

    Finished js and mobile version css for form 3.

---
+++ js/formThree.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const formCheckboxes = document.querySelectorAll('.div-add-on > input');
+
+  for (let i = 0; i < formCheckboxes.length; i++) {
+    const formCheckbox = formCheckboxes[i];
+
+    formCheckbox.addEventListener('click', (e) => {
+      if (formCheckbox.checked) {
+        e.target.parentElement.classList.add('selected');
+      } else {
+        e.target.parentElement.classList.remove('selected');
+      }
+    });
+  }
+});
